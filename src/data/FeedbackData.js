8c326d5ea1808302a30d6d56c96bdb893346aa03
Terry Babyuk <tbabyuk@gmail.com>
2022-07-23 01:49:03 -0400

    initial commit, feedback item list done

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,21 @@
+
+
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'This is item 1 text',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'This is item 2 text',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'This is item 3 text',
+    },
+  ]
+
+  export default FeedbackData;
